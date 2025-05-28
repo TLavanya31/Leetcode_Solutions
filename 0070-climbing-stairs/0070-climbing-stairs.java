fdaf6161e8ce4f78c34af398183ceabe5338eb93
Thadi Lavanya <lavanya310704@gmail.com>
2025-05-28 11:14:28 +0530

    Time: 0 ms (100%), Space: 40.4 MB (51.48%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -2,12 +2,12 @@ public class Solution {
     public int climbStairs(int n) {
         if (n <= 2) return n;
 
-        int first = 1;
-        int second = 2;
+        int first = 1;   // ways(1)
+        int second = 2;  // ways(2)
         int result = 0;
 
         for (int i = 3; i <= n; i++) {
-            result = first + second;
+            result = first + second;  // ways(n) = ways(n-1) + ways(n-2)
             first = second;
             second = result;
         }
