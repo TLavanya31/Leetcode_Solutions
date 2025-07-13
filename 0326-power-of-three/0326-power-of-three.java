5f87c5a4b4c40bfcfc3723c75cd97d03da9895e0
Thadi Lavanya <lavanya310704@gmail.com>
2025-07-13 21:33:45 +0530

    Time: 8 ms (84.65%), Space: 44 MB (67.85%) - LeetHub

--- 0326-power-of-three/0326-power-of-three.java
+++ 0326-power-of-three/0326-power-of-three.java
@@ -1,8 +1,5 @@
 class Solution {
     public boolean isPowerOfThree(int n) {
-        // if(n <= 0){
-        //     return false;
-        // }
         if(n == 1){
             return true;
         }
