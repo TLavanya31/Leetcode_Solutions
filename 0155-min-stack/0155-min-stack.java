7d33426b37a990c3ce19165a0abd6471eafd8a5f
Thadi Lavanya <thadilavanya31@gmail.com>
2026-01-13 21:59:56 +0530

    Time: 5 ms (83.36%), Space: 47.1 MB (51.32%) - LeetHub

---
+++ 0155-min-stack/0155-min-stack.java
@@ -0,0 +1,29 @@
+import java.util.Stack;
+
+class MinStack {
+
+    Stack<Integer> s = new Stack<>();
+    Stack<Integer> min = new Stack<>();
+
+    public void push(int x) {
+        s.push(x);
+        if (min.isEmpty() || x <= min.peek()) {
+            min.push(x);
+        }
+    }
+
+    public void pop() {
+        if (s.peek().equals(min.peek())) {
+            min.pop();
+        }
+        s.pop();
+    }
+
+    public int top() {
+        return s.peek();
+    }
+
+    public int getMin() {
+        return min.peek();
+    }
+}
